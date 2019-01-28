e163380efa23f1f72704f9b9116aefae4e77a300
tutscp <tutscp@gmail.com>
2019-01-28 17:44:39 -0500

    Deploy Next.js to gh-pages

---
+++ _next/static/rMLfWq0F7Djp9Ep7zR2wa/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{211:function(n,e,t){__NEXT_REGISTER_PAGE("/",function(){return n.exports=t(212),{page:n.exports.default}})},212:function(n,e,t){"use strict";t.r(e);var a=t(1),o=t.n(a),r=t(50);e.default=function(){return o.a.createElement(r.a,null,"Loading data from ","/website")}}},[[211,1,0]]]);
