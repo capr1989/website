f4fea4a69e36d14cfecc6565ebfee29e8dbedb6e
tutscp <tutscp@gmail.com>
2019-01-28 22:49:57 -0500

    Deploy Next.js to gh-pages

--- out/_next/static/development/pages/_app.js
+++ out/_next/static/development/pages/_app.js
@@ -9117,7 +9117,7 @@ module.exports = function(module) {
 /***/ (function(module, exports, __webpack_require__) {
 
 __NEXT_REGISTER_PAGE('/_app', function() {
-module.exports = __webpack_require__(/*! /Users/carlospretto/Desktop/webs-mac/gh-pages-app/node_modules/next/dist/pages/_app.js */"./node_modules/next/dist/pages/_app.js");
+module.exports = __webpack_require__(/*! /Users/carlospretto/Desktop/webs-mac/my-web/gh-pages-app/node_modules/next/dist/pages/_app.js */"./node_modules/next/dist/pages/_app.js");
 
 return { page: module.exports.default }});
 
