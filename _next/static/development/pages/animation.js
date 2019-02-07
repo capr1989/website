50edd46f0abde79fbd140b01d6f249e151ad66dd
tutscp <tutscp@gmail.com>
2019-02-06 20:35:16 -0500

    m

--- _next/static/development/pages/animation.js
+++ _next/static/development/pages/animation.js
@@ -63,7 +63,21 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _js_utils__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../js/utils */ "./js/utils.js");
 /* harmony import */ var _js_utils__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_js_utils__WEBPACK_IMPORTED_MODULE_4__);
 /* harmony import */ var _components_ActiveLink__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/ActiveLink */ "./components/ActiveLink.js");
+/* harmony import */ var emotion__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! emotion */ "./node_modules/emotion/dist/emotion.esm.js");
+/* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @emotion/styled */ "./node_modules/@emotion/styled/dist/styled.browser.esm.js");
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n              font-weight: ", ";\n            "]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -87,6 +101,13 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 
 
 
+
+
+
+if (typeof window !== "undefined") {
+  Object(emotion__WEBPACK_IMPORTED_MODULE_6__["hydrate"])(window.__NEXT_DATA__.ids);
+}
+
 /* harmony default export */ __webpack_exports__["default"] = (Object(next_router__WEBPACK_IMPORTED_MODULE_3__["withRouter"])(
 /*#__PURE__*/
 function (_React$Component) {
@@ -99,19 +120,6 @@ function (_React$Component) {
   }
 
   _createClass(AnimationLinks, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      // this.mountPathArr = window.location.pathname.split("/");
-      // this.mountPathLength = this.mountPathArr.length;
-      // this.mountQuery = this.mountPathArr[this.mountPathLength - 1];
-      var node = document.querySelectorAll(".cust__active.col.nav-link");
-      var classes = node[0].className; //   .split(" ")
-      //   .splice(1, 2)
-      //   .join(" ");
-      // node[0].className = classes;
-      // console.log(node[0].classList);
-    }
-  }, {
     key: "render",
     value: function render() {
       var _this = this;
@@ -120,28 +128,19 @@ function (_React$Component) {
         className: "col px-0"
       }, _js_globals__WEBPACK_IMPORTED_MODULE_2__["animationLinks"].map(function (p, key) {
         var linkName = Object(_js_utils__WEBPACK_IMPORTED_MODULE_4__["upperCaseLink"])(p.name);
-
-        var pathArr = _this.props.router.asPath.split("/");
-
-        var pathLength = _this.props.router.asPath.split("/").length;
-
-        var query = pathArr[pathLength - 1];
-
-        var classCondition = function classCondition() {
-          if (_this.props.router.query.name === p.name) {
-            return "cust__active col nav-link";
-          }
-
-          return "col nav-link";
-        };
-
+        var Li = _emotion_styled__WEBPACK_IMPORTED_MODULE_7__["default"].li(_templateObject(), function (_ref) {
+          var name = _ref.name,
+              query = _ref.query;
+          return name === query ? "bolder" : "300";
+        });
         return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
           key: key,
           href: "/animation?name=".concat(p.name),
           as: "".concat("", "/animation/").concat(p.name)
-        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
-          className: classCondition()
-        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, linkName)));
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Li, {
+          name: p.name,
+          query: _this.props.router.query.name
+        }, linkName)));
       }));
     }
   }]);
