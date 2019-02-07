ab74f8e08d724cce044965eebb245872652aa81d
tutscp <tutscp@gmail.com>
2019-02-06 20:35:16 -0500

    m

--- components/AnimationLinks.js
+++ components/AnimationLinks.js
@@ -4,44 +4,35 @@ import { animationLinks } from "../js/globals";
 import { withRouter } from "next/router";
 import { upperCaseLink } from "../js/utils";
 import ActiveLink from "../components/ActiveLink";
+import { hydrate } from "emotion";
+import styled from "@emotion/styled";
+
+if (typeof window !== "undefined") {
+  hydrate(window.__NEXT_DATA__.ids);
+}
 
 export default withRouter(
   class AnimationLinks extends React.Component {
-    componentDidMount() {
-      // this.mountPathArr = window.location.pathname.split("/");
-      // this.mountPathLength = this.mountPathArr.length;
-      // this.mountQuery = this.mountPathArr[this.mountPathLength - 1];
-      const node = document.querySelectorAll(".cust__active.col.nav-link");
-      const classes = node[0].className;
-      //   .split(" ")
-      //   .splice(1, 2)
-      //   .join(" ");
-      // node[0].className = classes;
-      // console.log(node[0].classList);
-    }
-
     render() {
       return (
         <ul className={"col px-0"}>
           {animationLinks.map((p, key) => {
             const linkName = upperCaseLink(p.name);
-            const pathArr = this.props.router.asPath.split("/");
-            const pathLength = this.props.router.asPath.split("/").length;
-            const query = pathArr[pathLength - 1];
-            const classCondition = () => {
-              if (this.props.router.query.name === p.name) {
-                return "cust__active col nav-link";
-              }
-              return "col nav-link";
-            };
+
+            const Li = styled.li`
+              font-weight: ${({ name, query }) =>
+                name === query ? "bolder" : "300"};
+            `;
             return (
               <Link
                 key={key}
                 href={`/animation?name=${p.name}`}
                 as={`${process.env.BACKEND_URL}/animation/${p.name}`}
               >
-                <a className={classCondition()}>
-                  <li>{linkName}</li>
+                <a>
+                  <Li name={p.name} query={this.props.router.query.name}>
+                    {linkName}
+                  </Li>
                 </a>
               </Link>
             );
--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f52e68170f623f204bfd";
+/******/ 	var hotCurrentHash = "53e111605e16eebd1056";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
