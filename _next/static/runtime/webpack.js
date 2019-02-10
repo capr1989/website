61f86cac4301c27bea53eb9cc4af078ea08a42f7
tutscp <tutscp@gmail.com>
2019-02-10 18:07:08 -0500

    Deploy Next.js to gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "74d1865ceeead33aca6a";
+/******/ 	var hotCurrentHash = "3d95760abb4782a496ee";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
