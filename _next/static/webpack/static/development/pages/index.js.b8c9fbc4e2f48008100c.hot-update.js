d44cfeb7e8a6406b3bfed614b584e1e640d3dad7
tutscp <tutscp@gmail.com>
2019-02-10 00:36:43 -0500

    m

---
+++ _next/static/webpack/static/development/pages/index.js.b8c9fbc4e2f48008100c.hot-update.js
@@ -0,0 +1,30 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./js/globals.js":
+/*!***********************!*\
+  !*** ./js/globals.js ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+exports.animationLinks = [{
+  name: "first"
+}, {
+  name: "particle"
+}, {
+  name: "second"
+}];
+exports.mainLinks = [{
+  name: "animation"
+}, {
+  name: "about"
+}, {
+  name: "contact"
+}, {
+  name: "story"
+}];
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.b8c9fbc4e2f48008100c.hot-update.js.map
