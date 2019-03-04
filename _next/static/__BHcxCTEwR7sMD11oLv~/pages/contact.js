7ec5800234747d17aa551ba0ec9e129b938eab07
tutscp <tutscp@gmail.com>
2019-03-04 00:35:58 -0500

    Deploy Next.js to gh-pages

---
+++ _next/static/__BHcxCTEwR7sMD11oLv~/pages/contact.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{250:function(n,t,e){__NEXT_REGISTER_PAGE("/contact",function(){return n.exports=e(251),{page:n.exports.default}})},251:function(n,t,e){"use strict";e.r(t);var c=e(0),o=e.n(c),a=e(8);t.default=Object(a.a)(function(n){return o.a.createElement("p",null,"Contact")})}},[[250,1,0]]]);
